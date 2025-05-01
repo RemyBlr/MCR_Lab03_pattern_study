e9440b4795eeec04d06299c199969700d7bb2a3e
RemyBlr <remy.bleuer@hotmail.com>
2025-05-01 21:37:12 +0200

    did the canvas layout

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import window.TDWindow;
 
 public class Main {
     public static void main(String[] args) {
-        // System.setProperty("apple.laf.useScreenMenuBar", "false");
         TDWindow window = TDWindow.getInstance();
     }
 }
--- src/window/TDWindow.java
+++ src/window/TDWindow.java
@@ -5,29 +5,34 @@ import java.awt.*;
 
 public class TDWindow {
     private static TDWindow instance;
-    private static final int width = 1500;
+    // Get screen size
+    private final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private final int width = (int) screenSize.getWidth();
+    private final int height = (int) screenSize.getHeight();
 
     private TDWindow() {
         // Main window
         JFrame frame = new JFrame("Paint Tower Defense");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(width, 700);
+        frame.setSize(width, height);
         frame.setLocationRelativeTo(null);
 
         // Upper menu
-        frame.add(createUpperMenu(), BorderLayout.NORTH);
+        frame.setJMenuBar(createUpperMenu());
 
-        // Left menu
-        JPanel shop = createShopMenu();
-        frame.add(shop);
-
-        // Canvas
-        frame.add(getJSplitPane(shop));
+        // Canvas + shop
+        JSplitPane splitPane = getJSplitPane();
+        frame.add(splitPane, BorderLayout.CENTER);
 
         // Show window
         frame.setVisible(true);
     }
 
+    /**
+     * Create the upper menu with "Option" and "Help" menus.
+     *
+     * @return JMenuBar
+     */
     private JMenuBar createUpperMenu() {
         JMenuBar menuBar = new JMenuBar();
 
@@ -44,6 +49,12 @@ public class TDWindow {
         return menuBar;
     }
 
+    /**
+     * Create the shop menu containing the gold coins and the different
+     * buying options.
+     *
+     * @return JPanel
+     */
     private JPanel createShopMenu() {
         JPanel shopPanel = new JPanel();
         shopPanel.setBackground(new Color(230, 230, 250));
@@ -60,22 +71,129 @@ public class TDWindow {
         return shopPanel;
     }
 
-    private static JSplitPane getJSplitPane(JPanel shop) {
+    /**
+     * Create the toolbar with different buttons.
+     * For example: Pen, Select, Colors...
+     *
+     * @return JToolBar
+     */
+    private JToolBar createToolBar() {
+        JToolBar toolBar = new JToolBar();
+        toolBar.setFloatable(false); // can't move the toolbar
+
+        JButton penButton = createIconButton("./img/pen.png", "Stylo");
+        JButton selectButton = createIconButton("./img/select.png", "Sélection");
+        JButton blackButton = createIconButton("./img/black.png", "Noir");
+        JButton blueButton = createIconButton("./img/blue.png", "Bleu");
+        JButton greenButton = createIconButton("./img/green.png", "Vert");
+        JButton redButton = createIconButton("./img/red.png", "Rouge");
+
+        toolBar.add(penButton);
+        toolBar.add(Box.createHorizontalStrut(10));
+        toolBar.add(selectButton);
+        toolBar.add(Box.createHorizontalStrut(10));
+        toolBar.add(blackButton);
+        toolBar.add(Box.createHorizontalStrut(10));
+        toolBar.add(blueButton);
+        toolBar.add(Box.createHorizontalStrut(10));
+        toolBar.add(greenButton);
+        toolBar.add(Box.createHorizontalStrut(10));
+        toolBar.add(redButton);
+
+        return toolBar;
+    }
+
+    /**
+     * Create the drawing canvas with a castle image and a red circle.
+     *
+     * @return JPanel
+     */
+    private JPanel createCanvas() {
+        ImageIcon castleIcon = new ImageIcon("./img/castle.png");
+        Image castleImage = castleIcon.getImage();
+        int castleWidth = 100;
+        int castleHeight = 100;
+        Image scaledCastleImage = castleImage.getScaledInstance(castleWidth, castleHeight, Image.SCALE_SMOOTH);
+
         JPanel canvasPanel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                g.setColor(Color.LIGHT_GRAY);
-                g.drawString("Drawing zone", 10, 20);
+                Graphics2D g2d = (Graphics2D) g.create();
+                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+                // Center coordinates for circle
+                int width = getWidth();
+                int height = getHeight();
+                int centerX = width / 2;
+                int centerY = height / 2;
+
+                // Red drawing zone
+                int radius = 200;
+                Color transparentRed = new Color(255, 0, 0, 27);
+                g2d.setColor(transparentRed);
+                g2d.fillOval(centerX - radius, centerY - radius, radius * 2, radius * 2);
+
+                // Castle
+                int imgX = centerX - castleWidth / 2;
+                int imgY = centerY - castleHeight / 2;
+                g2d.drawImage(scaledCastleImage, imgX, imgY, this);
+
+                g2d.dispose();
             }
         };
         canvasPanel.setBackground(Color.WHITE);
+
+        return canvasPanel;
+    }
+
+    /**
+     * Create the status bar at the bottom of the canvas.
+     *
+     * @return JPanel
+     */
+    private JPanel createStatusBar() {
+        JPanel statusBar = new JPanel(new BorderLayout());
+        statusBar.setBackground(new Color(245, 245, 245));
+        statusBar.setBorder(BorderFactory.createEmptyBorder(2, 5, 2, 5)); // marges fines
+
+        JLabel inkLabel = new JLabel("Encre: 100");
+        JLabel waveLabel = new JLabel("Vague: 1");
+        JLabel timeLabel = new JLabel("Temps: 00:00");
+
+        JPanel centerPanel = new JPanel();
+        centerPanel.setOpaque(false);
+        centerPanel.add(waveLabel);
+
+        statusBar.add(inkLabel, BorderLayout.WEST);
+        statusBar.add(centerPanel, BorderLayout.CENTER);
+        statusBar.add(timeLabel, BorderLayout.EAST);
+
+        return statusBar;
+    }
+
+    /**
+     * Create the main split pane with the canvas and the shop.
+     *
+     * @return JSplitPane
+     */
+    private JSplitPane getJSplitPane() {
+
+        JToolBar toolBar = createToolBar();
+        JPanel drawingZone = createCanvas();
+        JPanel statusBar = createStatusBar();
+        JPanel shopPanel = createShopMenu();
+
+        JPanel canvasPanel = new JPanel(new BorderLayout());
+        canvasPanel.add(toolBar, BorderLayout.NORTH);
+        canvasPanel.add(drawingZone, BorderLayout.CENTER);
+        canvasPanel.add(statusBar, BorderLayout.SOUTH);
 
         // Separation
         JSplitPane splitPane = new JSplitPane(
                 JSplitPane.HORIZONTAL_SPLIT,
                 canvasPanel,
-                shop
+                shopPanel
         );
         int threeQuarterWidth = width * 3 / 4;
         splitPane.setDividerLocation(threeQuarterWidth);
@@ -85,6 +203,35 @@ public class TDWindow {
         return splitPane;
     }
 
+
+    /**
+     * Create a button with an icon and a tooltip.
+     *
+     * @param iconPath Path to the icon image
+     * @param tooltip  Tooltip text
+     * @return JButton
+     */
+    private JButton createIconButton(String iconPath, String tooltip) {
+        ImageIcon icon = new ImageIcon(iconPath);
+
+        Image scaledImage = icon.getImage().getScaledInstance(24, 24, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+
+        JButton button = new JButton(scaledIcon);
+        button.setToolTipText(tooltip);
+
+        button.setBorderPainted(false);
+        button.setFocusPainted(false);
+        button.setContentAreaFilled(false);
+
+        return button;
+    }
+
+    /**
+     * Get the singleton instance of TDWindow.
+     *
+     * @return TDWindow instance
+     */
     public static TDWindow getInstance() {
         if(instance == null)
             instance = new TDWindow();
